01c915942875e997932ec82bb129646d117b7cc0
VariantCode <werecops21@gmail.com>
2022-12-01 05:13:29 -0500

    Part 2 solution/README update

--- day01/README.md
+++ day01/README.md
@@ -3,3 +3,10 @@ VariantCode
 
 The only .txt file that is needed to run this, is the cal-count.txt in the same folder as the shell 
 script.
+
+#### Linux users
+
+Make sure to make the script executable by running `sudo chmod+x cal-count.sh` before running the 
+command.
+
+To run the script open a terminal where you saved the file and run `./cal-count.sh`.
--- day01/cal-count.sh
+++ day01/cal-count.sh
@@ -10,20 +10,28 @@ tr '\n' '+' <cal-count.txt> cal-count-math.txt
 
 sed -i "s/++/\n/g" cal-count-math.txt
 
-# Perform the math on the calories and output it to the elf-cal-list and find the highest calories
+# Perform the math on the calories and output it to the elf-cal-list and sorting out the calories
 
 cat cal-count-math.txt | bc > elf-cal-list.txt && sort -n elf-cal-list.txt > sorted.txt
 
-tail -n1 sorted.txt
-
-#bc -q cal-count-math.txt > elf-cal-list.txt && sort -n elf-cal-list.txt > sorted.txt && tail -n1
-
-#cat elf-cal-list.txt | sort -n | tail -n1
-
-#touch sorted.txt
-
-#sort -n elf-cal-list.txt > sorted.txt
+# prints the highest value which happens to be at the end of the file
 
 #tail -n1 sorted.txt
+
+# Part 2; read the top three calorie holding elfs and write their values to a new file
+
+tail -n3 sorted.txt > top-3.txt
+
+# Translate the newlines into plus symbols
+
+tr '\n' '+' <top-3.txt> top-3-math.txt
+
+# add a zero to the end of the formula because bc doesn't like a + at the end
+
+echo "0" >> top-3-math.txt
+
+# Perform maths
+
+cat top-3-math.txt | bc
 
 exit
